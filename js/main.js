ef995ffaa689b68e07514f49bd8c9253f432ab86
elias alves <eliasalves.d.s2013@gmail.com>
2022-04-10 08:50:38 -0500

    atualizei

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const fila = document.querySelector('.contenedor-carousel');
+const peliculas = document.querySelectorAll('.pelicula');
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+// ? ----Event listener para la flecha derecha-------
+flechaDerecha.addEventListener('click', () => {
+     fila.scrollLeft += fila.offsetWidth;
+});
+
+// ? ----Event listener para la flecha izquierda-------
+flechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
+});
