6ec034fb5867a0b3cdc340fdbf0e9508006676f7
elias alves <eliasalves.d.s2013@gmail.com>
2022-04-11 21:17:55 -0500

    atualizei a lista

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const peliculas = document.querySelectorAll('.pelicula');
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
-
 //----lista 2
 const filab = document.querySelector('.contenedor-carouselb');
 const peliculasb = document.querySelectorAll('.peliculab');
@@ -14,12 +13,12 @@ const flechaDerechab = document.getElementById('flecha-derechab');
 
 
 
-// ? ----Event listener para la flecha derecha-------
+// ? ---- flecha derecha  lista 1-------
 flechaDerecha.addEventListener('click', () => {
      fila.scrollLeft += fila.offsetWidth;
 });
 
-// ? ----Event listener para la flecha izquierda-------
+// ? ---- flecha izquierda  lista 1  -------
 flechaIzquierda.addEventListener('click', () => {
     fila.scrollLeft -= fila.offsetWidth;
 });
@@ -27,12 +26,12 @@ flechaIzquierda.addEventListener('click', () => {
 
 
 
-// ? ----  lista 2  -------
+// ? ---- flecha derecha lista 2  -------
 flechaDerechab.addEventListener('click', () => {
     filab.scrollLeft += filab.offsetWidth;
 });
 
-// ? ----  lista 2 -------
+// ? ----  flecha izquierda  lista 2 -------
 flechaIzquierdab.addEventListener('click', () => {
    filab.scrollLeft -= filab.offsetWidth;
 });
